0290db31818e47ce1977680864c9b21775ad8763
wangxuuu <wangxucoco@live.com>
2019-12-23 14:04:41 +0800

    update for moving average

--- Adaptive_minee_entropy.py
+++ Adaptive_minee_entropy.py
@@ -1,20 +1,15 @@
-# %%
-
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from data.mix_gaussian import MixedGaussian
-import minee
-
-# %%
+import adaptive_minee
 
 if torch.cuda.is_available():
     torch.set_default_tensor_type(torch.cuda.FloatTensor)
 else:
     torch.set_default_tensor_type(torch.FloatTensor)
 
-# %%
-for d in range(1, 4):
+for d in range(1, 2):
     # d = 1  # d is the dimension of X and Y. The dimension of joint mix-gaussian distribution is 2*d
     rho = 0.9
     sample_size = 400
@@ -40,20 +35,6 @@ for d in range(1, 4):
         Y[:, i] = data[:, 1]
 
     data = np.append(X, Y, axis=1)
-
-    ref_entropy_XY = minee._uniform_sample_entropy(data)
-    ref_entropy_X = minee._uniform_sample_entropy(X)
-    ref_entropy_Y = minee._uniform_sample_entropy(Y)
-    # %%
-
-    # plt.figure()
-    # plt.scatter(X[:, 0], Y[:, 0])
-    # plt.xlabel('X')
-    # plt.ylabel('Y')
-    # plt.show()
-
-    # %%
-
     # -------------------------- Training ----------------------------- #
     # Using Neural Network to estimate the entropy of the generated Gaussian distribution
 
@@ -61,10 +42,10 @@ for d in range(1, 4):
     lr = 1e-4
 
     #####################
-    ref_batch_factor = 10  #
+    ref_batch_factor = 20  #
     ####################
 
-    NN = minee.MINEE(torch.Tensor(X), torch.Tensor(Y), batch_size=batch_size,ref_batch_factor=ref_batch_factor,lr=lr)
+    NN = adaptive_minee.MINEE(torch.Tensor(X), torch.Tensor(Y), batch_size=batch_size,ref_batch_factor=ref_batch_factor,lr=lr)
 
     num_iteration = 80000
 
@@ -78,27 +59,17 @@ for d in range(1, 4):
 
     for i in range(num_iteration):
         NN.step()
-        dXY, dX, dY = NN.forward()
-        entropy_XY.append(ref_entropy_XY - dXY)
-        entropy_X.append(ref_entropy_X - dX)
-        entropy_Y.append(ref_entropy_Y - dY)
-        MutualInfo.append(dXY - dX - dY)
+        dXY = NN.forward()
+        entropy_XY.append(- dXY)
         dXY_list.append(dXY)
-        dX_list.append(dX)
-        dY_list.append(dY)
-
-    ma_rate = 0.01            # rate of moving average
-    entropy_list = entropy_XY.copy()    # see also the estimate() member function of MINE
-    for i in range(1,len(entropy_list)):
-        entropy_list[i] = (1-ma_rate) * entropy_list[i-1] + ma_rate * entropy_list[i]
 
     plt.figure()
-    plt.plot(entropy_list, label='XY entropy')
+    plt.plot(entropy_XY, label='XY entropy')
     plt.axhline(h_xy, label='ground truth', linestyle='--', color='red')
     plt.xlabel('Iteration')
     plt.title('XY dim=%d' % (2 * d))
     plt.legend()
-    plt.savefig("./results/minee/dim=%d learnrate=%f.png" % (2 * d, lr))
+    plt.savefig("./results/dim=%d learnrate=%f.png" % (2 * d, lr))
     plt.show()
 
 
--- mine_entropy.py
+++ mine_entropy.py
@@ -1,19 +1,18 @@
-# %%
+#%%
 
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from data.mix_gaussian import MixedGaussian
-import minee
+import mine
 
-# %%
+#%%
 
 if torch.cuda.is_available():
     torch.set_default_tensor_type(torch.cuda.FloatTensor)
 else:
     torch.set_default_tensor_type(torch.FloatTensor)
 
-# %%
 for d in range(1, 4):
     # d = 1  # d is the dimension of X and Y. The dimension of joint mix-gaussian distribution is 2*d
     rho = 0.9
@@ -26,83 +25,73 @@ for d in range(1, 4):
 
     hx, hy, hxy, mi = mg.ground_truth
 
-    # ground truth of entropy of mixed gaussian distribution (X,Y)
-    h_xy = hxy * d
-    hx = hx * d
-    hy = hy * d
-
-    MI = mi * d
-
-    # (X,Y) is a mixed gaussian distribution; but X and Y are not.
+    """
+    Splitting the 2-d Mix-Gaussian data into 2 components, and assign them to X and Y respectively. 
+    Generate 2-d Mix-Gaussian data from the same distribution and repeat this step for d times. 
+    (X,Y) is a mixed gaussian distribution; but X and Y are not.
+    """
     for i in range(d):
         data = mg.data
         X[:, i] = data[:, 0]
         Y[:, i] = data[:, 1]
 
-    data = np.append(X, Y, axis=1)
+    import scipy.stats as st
+    pdf = st.multivariate_normal(
+                    mean=np.zeros(d),
+                    cov=np.identity(d))
+    density_x = pdf.pdf(X)   # p(x)
+    density_y = pdf.pdf(Y)   # p(y)
+    density_xy = density_x * density_y
+    # The cross entropy of the reference distribution, i.e. product of marginal distribution
+    H_crossentropy = -np.log(density_xy).mean()
 
-    ref_entropy_XY = minee._uniform_sample_entropy(data)
-    ref_entropy_X = minee._uniform_sample_entropy(X)
-    ref_entropy_Y = minee._uniform_sample_entropy(Y)
-    # %%
+    #%%
 
-    # plt.figure()
-    # plt.scatter(X[:, 0], Y[:, 0])
-    # plt.xlabel('X')
-    # plt.ylabel('Y')
-    # plt.show()
+    # Ground truth of entropy of mixed gaussian distribution (X,Y)
+    # As the 2-d mix-gaussian data is generated independently, the entropy of (X, Y) is d times
+    # the entropy of the 2-d mix-gaussian data.
+    h_xy = hxy * d
 
-    # %%
+    #%%
 
     # -------------------------- Training ----------------------------- #
     # Using Neural Network to estimate the entropy of the generated Gaussian distribution
 
     batch_size = 100
     lr = 1e-4
+    ma_rate = 0.1
 
-    #####################
-    ref_batch_factor = 10  #
-    ####################
+    NN = mine.MINE(torch.Tensor(X),torch.Tensor(Y),batch_size=batch_size,lr=lr,ma_rate=ma_rate)
 
-    NN = minee.MINEE(torch.Tensor(X), torch.Tensor(Y), batch_size=batch_size,ref_batch_factor=ref_batch_factor,lr=lr)
-
-    num_iteration = 80000
+    num_iteration = 200000
 
     entropy_XY = []
-    entropy_X = []
-    entropy_Y = []
-    MutualInfo = []
     dXY_list = []
-    dX_list = []
-    dY_list = []
 
     for i in range(num_iteration):
         NN.step()
-        dXY, dX, dY = NN.forward()
-        entropy_XY.append(ref_entropy_XY - dXY)
-        entropy_X.append(ref_entropy_X - dX)
-        entropy_Y.append(ref_entropy_Y - dY)
-        MutualInfo.append(dXY - dX - dY)
+        dXY = NN.forward()
+        entropy_XY.append(H_crossentropy - dXY)
         dXY_list.append(dXY)
-        dX_list.append(dX)
-        dY_list.append(dY)
+
+    #%%
 
     ma_rate = 0.01            # rate of moving average
     entropy_list = entropy_XY.copy()    # see also the estimate() member function of MINE
     for i in range(1,len(entropy_list)):
         entropy_list[i] = (1-ma_rate) * entropy_list[i-1] + ma_rate * entropy_list[i]
 
+    #%%
+
+    # plot the estimate entropy
     plt.figure()
     plt.plot(entropy_list, label='XY entropy')
     plt.axhline(h_xy, label='ground truth', linestyle='--', color='red')
     plt.xlabel('Iteration')
-    plt.title('XY dim=%d' % (2 * d))
+    plt.title('XY dim=%d, learnrate=%f' % (2 * d, lr))
     plt.legend()
-    plt.savefig("./results/minee/dim=%d learnrate=%f.png" % (2 * d, lr))
+    plt.savefig("./results/mine/dim=%d learnrate=%f.png" % (2*d,lr))
     plt.show()
-
-
-
 
 
 
